bde708f365b123d2fee2aaa209114ac8f1ab1c79
Vishal Sunak <VMoney@Vishals-iMac.local>
2012-12-26 21:25:12 -0500

    Initial Build

---
+++ InstaPhoto/AppDelegate.m
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.m
+//  InstaPhoto
+//
+//  Created by Vishal Sunak on 12/26/12.
+//  Copyright (c) 2012 Vishal Sunak. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    //This is the initialization of the tab controller:
+    UITabBarController *tabBarController = [[UITabBarController alloc] init];
+    
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    
+    
+    //Now Define the ViewController that will be used:
+
+    //FeedController that shows up as "Feed" in the tab at the bottom
+    UIViewController *feedViewController = [[UIViewController alloc] init];
+    feedViewController.title = @"Feed";
+    feedViewController.tabBarItem.image = [UIImage imageNamed:@"fire.png"];
+    
+    //FavoritesController that shows up as "Favorites" in the tab at the bottom
+    UIViewController *favoritesViewController = [[UIViewController alloc] init];
+    favoritesViewController.title = @"Favorites";
+    feedViewController.tabBarItem.image = [UIImage imageNamed:@"tab_icon_feed"];
+    
+    //Set the background colors of each of our Controllers that are in the tab
+    feedViewController.view.backgroundColor = [UIColor blueColor];
+    favoritesViewController.view.backgroundColor = [UIColor redColor];
+    
+    //Adds the view controllers to the tabBarController
+    [tabBarController setViewControllers:@[feedViewController, favoritesViewController]];
+    
+    
+    //Icons: 
+    
+    //Set the rootViewController to the tabController:
+    self.window.rootViewController = tabBarController;
+    
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+
+
+@end
